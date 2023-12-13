8aebbce54eaf9bc5e06566b591e0d048f952e7cd
Liudmila Portnova <portnovaliudmila@gmail.com>
2023-12-13 11:00:07 +0300

    refactorÐ: add the BaseModel and the @FindBy annotation (#4)

--- src/test/java/practice/pineapple/model/FeedbackPage.java
+++ src/test/java/practice/pineapple/model/FeedbackPage.java
@@ -1,26 +1,22 @@
 package practice.pineapple.model;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import practice.pineapple.model.base.BasePage;
 
 public class FeedbackPage extends BasePage {
 
-    private final By HEDER_REVIEWS = By.xpath("//h1");
+    @FindBy(xpath = "//h1")
+    private WebElement heder1;
 
     public FeedbackPage(WebDriver driver) {
 
         super(driver);
     }
 
-    public WebElement getHederReviews() {
-
-        return getDriver().findElement(HEDER_REVIEWS);
-    }
-
     public String getTextHederReviews() {
 
-        return getText(getHederReviews());
+        return getText(heder1);
     }
 }
